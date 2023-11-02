40f1c76358ab2e2fb9fe060d980883fcfb56cedd
Nicole Garcia <nikkgarcia299@gmail.com>
2023-11-02 14:18:30 -0300

    Casi completo

---
+++ js.js
@@ -0,0 +1,14 @@
+const testimonios = document.querySelectorAll('.testimonial');
+let index = 0;
+
+function mostrarSiguienteTestimonio() {
+    testimonios[index].classList.remove('active');
+    index = (index + 1) % testimonios.length;
+    testimonios[index].classList.add('active');
+}
+
+function mostrarTestimonioAutomaticamente() {
+    mostrarSiguienteTestimonio();
+}
+
+setInterval(mostrarTestimonioAutomaticamente, 5000);
